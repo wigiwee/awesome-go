bdba8510c60d583ec6ccbb5b08ae5d80c4b301c4
wigiwee <kaustubhkolhe822@outlook.com>
2025-07-20 15:39:26 +0530

    working with actors

--- advance/actors/main.go
+++ advance/actors/main.go
@@ -3,13 +3,40 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/anthdm/hollywood/actor"
 )
 
 type Player struct {
-	HP int
+	HP           int
+	inventoryPid *actor.PID
+}
+
+type Inventory struct {
+	Bottles int
+}
+
+type DrinkBottle struct {
+	amount int
+}
+
+func NewInventory(bottles int) actor.Producer {
+	return func() actor.Receiver {
+		return &Inventory{
+			Bottles: bottles,
+		}
+	}
+}
+
+func (i *Inventory) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case actor.Started:
+		fmt.Println("Inventory actor is started (read state from DB) ")
+		_ = msg
+	case *actor.Stopped:
+		fmt.Println("Inventory actor stopped(save state to DB)")
+
+	}
 }
 
 func NewPlayer(hp int) actor.Producer {
@@ -31,8 +58,11 @@ func (p *Player) Receive(c *actor.Context) {
 		fmt.Println("Player is taking damage ", msg.amount)
 	case actor.Started:
 		fmt.Println("player actor is started (read state from db)")
+		p.inventoryPid = c.SpawnChild(NewInventory(1), "inventory")
 	case actor.Stopped:
 		fmt.Println("Player actor is stopped (save state to db)")
+	case DrinkBottle:
+
 	}
 }
 
@@ -46,10 +76,11 @@ func main() {
 
 	msg := takeDamage{amount: 333}
 
-	e.Send(pid, msg)
+	_ = msg
+	//e.Send(pid, msg)
 
 	//because e.Send is non blocking method
-	time.Sleep(20 * time.Millisecond)
+	//time.Sleep(20 * time.Millisecond)
 
 	fmt.Println("process id ", pid)
 
